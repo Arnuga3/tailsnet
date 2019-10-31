46567aa841f4ea21d9a7ac819e75ad46914ba445
Arnuga3 <20136310@nescol.ac.uk>
2019-10-31 23:41:20 +0000

    Add domain

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -11,7 +11,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
+  callbackURL: "https://tailsnet.herokuapp.com/auth/facebook/callback"
 },
 (accessToken, refreshToken, profile, done) => {
   console.log(accessToken);
@@ -23,7 +23,7 @@ passport.use(new FacebookStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://tailsnet.herokuapp.com/auth/google/callback"
 },
 (accessToken, refreshToken, profile, done) => {
   onsole.log(accessToken);
